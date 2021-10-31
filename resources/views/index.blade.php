97ca07406f33dd35fe737bf96155b922a631741c
numahatake <ynumahata@gmail.com>
2021-11-01 06:43:48 +0900

    2021/11/01-Laravel-OA

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="/posts/{{ $index }}">
+                        {{ $post }}
+                    </a>
+                </li>
+            @empty
+            <li>No posts yet!</li>
+            @endforelse
+        </ul>
+    </div>
+</body>
+</html>
