58bcbfc890598e6886c5c7795d4dfdabb3a2c1a0
numahatake <ynumahata@gmail.com>
2021-11-02 06:58:01 +0900

    2021/11/02-Laravel-OA

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,13 +6,19 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    private $posts = [
+        'Title A',
+        'Title B',
+        'Title C',
+    ];
+
     public function index()
     {
-        $posts = [
-            'Title A',
-            'Title B',
-            'Title C',
-        ];
-        return view('index')->with(['posts' => $posts]);
+        return view('index')->with(['posts' => $this->posts]);
+    }
+
+    public function show($id)
+    {
+        return view('posts.show')->with(['post' => $this->posts[$id]]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use App\Http\Controllers\PostController;
 |
 */
 
-// Route::get('/', ['App\Http\Controllers\PostController', 'index']);
-// Route::get('/', [App\Http\Controllers\PostController::class, 'index']);
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
