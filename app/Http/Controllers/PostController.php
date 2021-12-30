3a1dee1ee75f441aa27661412b9a7ecb9e020d76
numahatake <ynumahata@gmail.com>
2021-12-30 10:11:03 +0900

    Controllerでデータを取得2021/12/30

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    private $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-    ];
-
     public function index()
     {
-        return view('index')->with(['posts' => $this->posts]);
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+
+        return view('index')->with(['posts' => $posts]);
     }
 
     public function show($id)
